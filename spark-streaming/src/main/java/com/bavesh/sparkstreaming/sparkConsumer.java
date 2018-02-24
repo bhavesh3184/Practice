c2b6dfa6cb9983cc857cc056185797a03d3c4f9c
bhavesh <bhavesh3184@gmail.com>
2018-02-24 16:11:00 +0000

    general files

--- spark-streaming/src/main/java/com/bavesh/sparkstreaming/sparkConsumer.java
+++ spark-streaming/src/main/java/com/bavesh/sparkstreaming/sparkConsumer.java
@@ -1,4 +1,74 @@
 package com.bavesh.sparkstreaming;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.spark.SparkConf;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
+import scala.Tuple2;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
 public class sparkConsumer {
+
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args)  throws Exception{
+
+       /* if (args.length < 2) {
+            System.err.println("Usage: JavaDirectKafkaWordCount <brokers> <topics>\n" +
+                    "  <brokers> is a list of one or more Kafka brokers\n" +
+                    "  <topics> is a list of one or more kafka topics to consume from\n\n");
+            System.exit(1);
+        }*/
+
+
+
+        String brokers = "127.0.0.1:9092";
+        String topics = "wordcount2402";
+
+        // Create context with a 2 seconds batch interval
+        SparkConf sparkConf = new SparkConf().setAppName("JavaDirectKafkaWordCount").setMaster("local[*]");
+
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(2));
+
+        Set<String> topicsSet = new HashSet<>(Arrays.asList(topics.split(",")));
+        Map<String, Object> kafkaParams = new HashMap<>();
+
+        kafkaParams.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,brokers);
+        kafkaParams.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        kafkaParams.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,StringDeserializer.class.getName());
+
+        kafkaParams.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"earliest");
+        kafkaParams.put(ConsumerConfig.GROUP_ID_CONFIG,"2402");
+        kafkaParams.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false");
+
+
+
+        // Create direct kafka stream with brokers and topics
+        JavaInputDStream<ConsumerRecord<String, String>> messages = KafkaUtils.createDirectStream(
+                jssc,
+                LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.Subscribe(topicsSet, kafkaParams));
+
+        // Get the lines, split them into words, count the words and print
+        JavaDStream<String> lines = messages.map(ConsumerRecord::value);
+        JavaDStream<String> words = lines.map(x ->x);
+        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(s -> new Tuple2<>(s, 1))
+                .reduceByKey((i1, i2) -> i1 + i2);
+        wordCounts.print();
+
+        // Start the computation
+        jssc.start();
+        jssc.awaitTermination();
+
+    }
 }
